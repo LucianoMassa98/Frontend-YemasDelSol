1b3ad95e7ebf98bf0677a97fe4fda6a9b71f18b1
franpa13 <frandelregue@gmail.com>
2024-05-01 02:41:43 -0300

    terminando maquetado del nuevo figma

--- src/components/ActionAreaCard/ActionAreaCard.jsx
+++ src/components/ActionAreaCard/ActionAreaCard.jsx
@@ -4,6 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import TextField from '@mui/material/TextField';
 import CardMedia from '@mui/material/CardMedia';
 import FormControl from '@mui/material/FormControl';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
@@ -25,20 +26,23 @@ export default function ActionAreaCard({ img, nombre, precio, setAddToCart }) {
     };
     return (
         <Card sx={{
-            borderRadius: "15px",
-            width: "30%", '@media (max-width: 620px)': {
+            borderRadius: "10px",
+            width: "19%", '@media (max-width: 620px)': {
                 width: "90%",
             },
+            boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.4)",
+            marginY: "15px",
         }}>
-            <CardActionArea sx={{ display: "flex" }}>
+            <CardActionArea sx={{ display: "flex", flexDirection: "column" }}>
                 <CardMedia
                     component="img"
-                    height="210"
+                    height="180"
                     image={img}
                     sx={{
-                        padding: "22px",
+                        paddingTop: "16px",
+                        backgroundColor: "#F7A600",
                         display: "flex",
-                        borderRadius: "35px",
+                        borderRadius: "0px",
                         // backgroundColor: "red",
 
                         justifyContent: "center",
@@ -47,27 +51,44 @@ export default function ActionAreaCard({ img, nombre, precio, setAddToCart }) {
                     }} alt={nombre}
 
                 />
-                <CardContent sx={{ backgroundColor: "#E8E8E8", display: "flex", justifyContent: "start", padding: "20px" }}>
-                    <Box sx={{ minWidth: 180, display: "flex", flexDirection: "column", alignItems: "start" }}>
-                        <Typography gutterBottom variant="h5" component="div">
+                <CardContent sx={{ backgroundColor: "white", display: "flex", justifyContent: "start", padding: "20px" }}>
+                    <Box sx={{ minWidth: 180, display: "flex", flexDirection: "column", gap: "15px", alignItems: "start" }}>
+                        <Typography gutterBottom variant="h5" sx={{ fontWeight: "bold" }} component="div">
                             {nombre}
                         </Typography>
 
-                        <TextField id="outlined-basic" label="Ingrese una cantidad" InputProps={{
-                            sx: {
-                                borderRadius: '80px', // Ajusta el valor según sea necesario
-                                backgroundColor: 'white',
-                                border: 'none'
-                            },
-                        }} variant="outlined" value={cantidad} onChange={handleChange} />
                         <Typography sx={{ color: "#0019F7" }} gutterBottom variant="h5" component="div">
                             ${precio}
                         </Typography>
-                        <Button onClick={addToCart} sx={{
-                            backgroundColor: "#FFA500", width: "100%", fontSize: "13px", '&:hover': {
-                                backgroundColor: "#FFD100",
-                            }
-                        }} variant="contained">Agregar al carrito</Button>
+                        <Box sx={{ display: "flex", justifyContent: "center", gap: "0px", alignItems: "center", width: "100%" }}>
+                            <span style={{ width: "65%" }}>
+                                Cantidad :
+                            </span>
+                            <TextField type='number' size='small' id="outlined-basic" label="" InputProps={{
+                                sx: {
+                                    width: "80%",
+                                    backgroundColor: 'white',
+                                    border: 'none'
+                                },
+                            }} variant="outlined" value={cantidad} onChange={handleChange} />
+                        </Box>
+                        <Button
+                            onClick={addToCart}
+                            startIcon={<ShoppingCartOutlinedIcon />} 
+                            variant="contained"
+                            sx={{
+                                backgroundColor: "#FFA500",
+                                width: "100%",
+                                fontSize: "13px",
+                                '&:hover': {
+                                    backgroundColor: "#FFD100",
+                                },
+                                color: "black",
+                                fontWeight: "700"
+                            }}
+                        >
+                            Agregar al carrito
+                        </Button>
 
 
                     </Box>
--- src/pages/Vendedor/homeVendedor/HomeVendedor.jsx
+++ src/pages/Vendedor/homeVendedor/HomeVendedor.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import Autocomplete from '@mui/material/Autocomplete';
 import Select from '@mui/material/Select';
 import axios from 'axios';
 import MenuItem from '@mui/material/MenuItem';
@@ -25,7 +26,18 @@ export default function HomeVendedor() {
     const [cantidad, setCantidad] = useState("1")
     const [huevos, setHUevos] = useState([])
     const [loading, setLoading] = useState(true);
-
+    const top100Films = [
+        { label: 'The Shawshank Redemption', year: 1994 },
+        { label: 'The Godfather', year: 1972 },
+        { label: 'The Godfather: Part II', year: 1974 },
+        { label: 'The Dark Knight', year: 2008 },
+        { label: '12 Angry Men', year: 1957 },
+        { label: "Schindler's List", year: 1993 },
+        { label: 'Pulp Fiction', year: 1994 },
+        {
+            label: 'The Lord of the Rings: The Return of the King',
+            year: 2003,
+        },]
     const [addToCart, setAddToCart] = useState([])
     const handleOpen = () => setOpenModal(true);
     const handleClose = () => setOpenModal(false);
@@ -66,44 +78,64 @@ export default function HomeVendedor() {
 
                     </div>
                     <div className='filtrado-prod'>
-                        <div className='div-prod' >
-
-
-                            <label htmlFor="">Ingrese un nombre o codigo</label>
-
-                            <div className='cont-input'>
-
-                                <input type="text" placeholder='Ingrese nombre o codigo' />
-
-                                <SearchIcon sx={{ fontSize: "18px" }} />
-                            </div>
-                        </div>
-                        <div className='cantidad' >
-                            <label htmlFor="">Ingrese una cantidad</label>
-
-                            <input
-                                type="number"
-                                placeholder='Cantidad'
-                                value={cantidad}
-                                onChange={handleChange}
-                            />
-                        </div>
                         <div className='cont-select'>
                             <label htmlFor="">
-                                Ingrese un descuento
+                                Producto :
                             </label>
-                            <select name="" id="">
-                                <option value="" disabled selected>Descuento</option>
-                                <option value="">10%</option>
-                                <option value="">20%</option>
-                                <option value="">30%</option>
+                            <Autocomplete
+                                size="small"
+                                disablePortal
+                                id="combo-box-demo"
+                                options={top100Films}
+                                sx={{ width: 270 }}
+                                renderInput={(params) => <TextField {...params} placeholder="Ingrese numero o codigo" />}
+                            />
+                        </div>
 
-                            </select>
+                        <div className='cantidad' >
+                            <label htmlFor="">Cantidad :</label>
+
+
+                            <TextField
+                                type='number'
+                                id="outlined-basic"
+                                variant="outlined"
+                                value={cantidad}
+                                onChange={handleChange}
+                                sx={{ width: "50%" }}
+                                size="small"
+                                InputProps={{
+
+                                    sx: { p: "0px" }, // Otros estilos específicos para los InputProps
+                                }}
+                            />
                         </div>
                         <div className='cont-btn'>
 
                             <Button variant='contained' color='success'>Agregar</Button>
                         </div>
+                        <div className='div-prod' >
+
+
+                            <label htmlFor="">Buscar </label>
+
+
+                            <TextField
+                                id="outlined-basic"
+                                variant="outlined"
+                                size="small"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <SearchIcon sx={{ fontSize: "18px" }} />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+
+
+                        </div>
+
                     </div>
                 </section>
                 <section className='section-cards' >
@@ -115,35 +147,35 @@ export default function HomeVendedor() {
                         ))
                     ) : (
                         huevos.map((huevo, i) => (
-                            i < 6 && (
 
-                                <ActionAreaCard
-                                    addToCart={addToCart}
-                                    setAddToCart={setAddToCart}
-                                    cantidad={cantidad}
-                                    handleChange={handleChange}
-                                    setCantidad={setCantidad}
-                                    key={huevo.id}
-                                    nombre={huevo.nombre}
-                                    precio={huevo.precio}
-                                    img={"https://th.bing.com/th/id/R.4298670ef7a0bccdacd20c6c6c7ad367?rik=1KPhok5EbBVWRg&pid=ImgRaw&r=0"}
-                                />
-                            )
+
+                            <ActionAreaCard
+                                addToCart={addToCart}
+                                setAddToCart={setAddToCart}
+                                cantidad={cantidad}
+                                handleChange={handleChange}
+                                setCantidad={setCantidad}
+                                key={huevo.id}
+                                nombre={huevo.nombre}
+                                precio={huevo.precio}
+                                img={"https://s3-alpha-sig.figma.com/img/2419/719c/5ef5720880f32546147fd2bd3e66854b?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EMIJdi4O~9DB4PnQKnaI4Q3PfAnqE9OlmBwNKebaRKYTk6IgXtaHd8lFsLHbLYDTQgAaI2n~PEMuDeh1zrw2RfQ9g6CcqiDd10IchhnZ9-MW15Jjz6nietK4Nwf0DqYf5tA6~zBGaL9YRLtzKG2PQEpXNx5lFcnkJlmsb2kZb4ihScqhDnrw2BNA75pTEtUTSNei1gA9Uei4aeoYT68Xkb1LJexChMxsmCv7AaxJRZ8oyNEb8gp5NjU2MQNlJKDYl9t2rN8ouqVfalML1fMg0v6orq2J-YCcUJ04fkJF-ARgjw1yTgPR0sFdTOwbJj8Go~2MWe7o2jWAPQE1kflQyQ__"}
+                            />
+
                         ))
                     )}
                 </section>
 
-                <div className='btn-ver-mas'>
+                {/* <div className='btn-ver-mas'>
                     <Button sx={{
                         backgroundColor: "#6170FA", width: "30%", height: "40px", fontSize: "13px", '&:hover': {
                             backgroundColor: "#6170FA",
                         }
                     }} variant="contained">Ver mas</Button>
-                </div>
+                </div> */}
 
                 {/* <svg className='wave' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,288L16,261.3C32,235,64,181,96,165.3C128,149,160,171,192,202.7C224,235,256,277,288,293.3C320,309,352,299,384,277.3C416,256,448,224,480,197.3C512,171,544,149,576,160C608,171,640,213,672,208C704,203,736,149,768,144C800,139,832,181,864,176C896,171,928,117,960,80C992,43,1024,21,1056,58.7C1088,96,1120,192,1152,234.7C1184,277,1216,267,1248,245.3C1280,224,1312,192,1344,160C1376,128,1408,96,1424,80L1440,64L1440,0L1424,0C1408,0,1376,0,1344,0C1312,0,1280,0,1248,0C1216,0,1184,0,1152,0C1120,0,1088,0,1056,0C1024,0,992,0,960,0C928,0,896,0,864,0C832,0,800,0,768,0C736,0,704,0,672,0C640,0,608,0,576,0C544,0,512,0,480,0C448,0,416,0,384,0C352,0,320,0,288,0C256,0,224,0,192,0C160,0,128,0,96,0C64,0,32,0,16,0L0,0Z"></path></svg> */}
             </div>
-            <div className='contenedor-svg'></div>
+
             {!openModal && (
                 <div onClick={handleOpen} className="cart-icon-container">
                     {
