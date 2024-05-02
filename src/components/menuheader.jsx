2655aee0908b106967a4a4b54bfd21ff3eb665a9
Martin-M42 <83964814+Martin-M42@users.noreply.github.com>
2024-05-02 18:05:09 -0300

    Use navigate (#20)
    * cambios en menuheader
    
    * cambios en la estetica del header y agregue el nombre del admin en la seccion galpon
    
    * agregue el aviso de admin en el componente /adminmenu
    
    * agregue el layout con los links
    
    * cambie detalles de los links e hice responsiva para pantallas pequeñas
    
    ---------
    
    Co-authored-by: Githubmagali <magalivictoria85068@gmail.com>

--- src/components/menuheader.jsx
+++ src/components/menuheader.jsx
@@ -1,61 +1,87 @@
-import { IconButton } from "@mui/material";
-import DehazeIcon from "@mui/icons-material/Dehaze";
 import "./menuheader.css";
 import { useState } from "react";
 import { RightDrawer } from "./drawers/right-drawer";
 import { useStore } from "../store/use-store";
-
-
+import { Loader } from "../pages/login/loader";
+import { Link } from "react-router-dom";
+import { LinkCard } from "../common/linkcard";
+import GroupIcon from "@mui/icons-material/Group";
+import HardwareIcon from "@mui/icons-material/Hardware";
+import WarehouseIcon from "@mui/icons-material/Warehouse";
 
 
 
 export const Menuheader = () => {
   const [isopen, setIsopen] = useState(false);
+
+
   const logout = useStore((state) => state.doLogout);
   const loggeduser = useStore((state) => state.user);
+  const loggedUser = useStore((state) => state.user);
 
-  const handleOpening = () => {
-    if (isopen) {
-      setIsopen(false);
-    } else {
-      setIsopen(true);
-    }
-  };
+  const [menuVisible, setMenuVisible] = useState(false);
 
-  const onClose = () => {
-    setIsopen(false);
-  };
 
+  Loader("admin");
   return (
-    <div className="menu-header">
-      <p  onClick={handleOpening} className="button-menu">
-        <DehazeIcon sx={{ fontSize: "inherit"}}  />
-      </p>
-      <RightDrawer
-        PaperProps={{
-          sx: { backgroundColor: "orange", height: 200, borderRadius: 8, textDecoration: 'none' },
-        }}
-        anchor="left"
-        open={isopen}
-        onClose={onClose}
-      >
-        <div className="burgercontent">
-          <img src="/yemaslogo.jpeg" width="70px" height="70px"></img>
-          {/*<EggIcon sx={{ fontSize: 40, zIndex: 1201 }} />*/}
-         
-     
+    <>
+      <div className="home">
+        <div className="menu-hamburguer"
+          onClick={() => setMenuVisible(!menuVisible)}
+        >
+          &#9776;
         </div>
         
-        <div className="burgeroptions">
-          <p className="burgeroption" onClick={() => logout()}>
-            Cerrar sesión
-          </p>
-          <div className="border-line"></div>
-
-          <p className="burgeroption">Soporte</p>
-          <div className="border-line"></div>
+        <div className="menu-img">
+          <img src="yemaslogo.jpeg" width={56} height={56} />
         </div>
-      </RightDrawer>
-    </div>
+      </div>
+      <div>
+        {menuVisible &&
+          <div className="menu-desplegable">
+
+            <div>
+              {loggedUser && loggedUser.roleId === 1 && (
+                <>
+                  <div className="menu-container-main">
+                    <div className="menu-container">
+                      <Link to="/adminmenu/production" color="inherit" underline="none">
+                        <h4>  <HardwareIcon />Produccion</h4>
+
+                      </Link>
+                    </div>
+
+                    <div className="menu-container">
+                      <Link to="/adminmenu/users" color="inherit" underline="none">
+                        <h4 > <GroupIcon />Usuarios</h4>
+
+                      </Link>
+                    </div>
+                  
+                    <div className="menu-container">
+                      <Link to="/adminmenu/galpones" color="inherit" underline="none">
+                        <h4 ><WarehouseIcon />Galpones</h4>
+
+                      </Link>
+                    </div>
+                    <div className="burgeroptions">
+                      <p className="burgeroption" onClick={() => logout()}>
+                        Cerrar sesión
+                      </p>
+                      <div className="border-line"></div>
+
+                      <p className="burgeroption">Soporte</p>
+                      <div className="border-line"></div>
+                    </div>
+
+                  </div>
+
+                </>
+              )}
+            </div>
+      
+          </div>}
+      </div>
+    </>
   );
 };
